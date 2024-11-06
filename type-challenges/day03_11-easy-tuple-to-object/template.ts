77e676e15098287e2417344bd810084abf6c74ba
Lenny <ly2484579947@gmail.com>
2024-11-06 11:15:37 +0800

    bugfix: tuple-key-constraint

--- type-challenges/day03_11-easy-tuple-to-object/template.ts
+++ type-challenges/day03_11-easy-tuple-to-object/template.ts
@@ -1,7 +1,7 @@
 // https://github.com/type-challenges/type-challenges/blob/main/questions/00011-easy-tuple-to-object/README.md
 
-export type TupleToObject<T extends readonly any[]> = {
-  [P in T[number]]: P;
+export type TupleToObject<T extends readonly (string | number | symbol)[]> = {
+  [P in T[number]]:  P;
 };
 
 // 1.typeof: TypeScript adds a typeof operator you can use in a type context to refer to the type of a variable or property , connect ts and js
---
+++ type-challenges/day04_14-easy-first/template.ts
@@ -0,0 +1,8 @@
+// https://github.com/type-challenges/type-challenges/tree/main/questions/00014-easy-first
+type First<T extends any[]> = any
+
+type arr1 = ['a', 'b', 'c']
+type arr2 = [3, 2, 1]
+
+type head1 = First<arr1> // expected to be 'a'
+type head2 = First<arr2> // expected to be 3
