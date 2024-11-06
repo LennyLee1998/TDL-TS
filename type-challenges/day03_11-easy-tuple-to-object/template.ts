7ddfa28213b560932b914dc7e08fb8c670226607
Lenny <ly2484579947@gmail.com>
2024-11-06 11:05:21 +0800

    easy-tuple

--- type-challenges/day03_11-easy-tuple-to-object/template.ts
+++ type-challenges/day03_11-easy-tuple-to-object/template.ts
@@ -1,8 +1,18 @@
 // https://github.com/type-challenges/type-challenges/blob/main/questions/00011-easy-tuple-to-object/README.md
 
-type TupleToObject<T extends readonly any[]> = any
+export type TupleToObject<T extends readonly any[]> = {
+  [P in T[number]]: P;
+};
 
+// 1.typeof: TypeScript adds a typeof operator you can use in a type context to refer to the type of a variable or property , connect ts and js
 
-const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+// 2.literal types
+// constant => typeof => literal types
+const str = "234";
+type StrType = typeof str;
 
-type result = TupleToObject<typeof tuple> // expected { 'tesla': 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
+// 3. as const: tuple element can not be modified
+
+const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
+
+type result = TupleToObject<typeof tuple>; // expected { 'tesla': 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
