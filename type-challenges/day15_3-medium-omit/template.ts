1204c1a52ec55818ac1ee37301d45bd36e77c774
Lenny <ly2484579947@gmail.com>
2024-11-17 07:23:32 +0800

    medium-omit

--- type-challenges/day15_3-medium-omit/template.ts
+++ type-challenges/day15_3-medium-omit/template.ts
@@ -2,16 +2,29 @@
 // Implement the built-in Omit<T, K> generic without using it.
 
 // Constructs a type by picking all properties from T and then removing K
-export type MyOmit<T, K> = any
-
+// prefix readonly 怎么处理
+// as 语法
+// 这是 TypeScript 4.1 引入的键重映射（key remapping）特性
+// 允许我们在映射过程中转换属性名, 允许你通过条件过滤来创建新的键：
+// export type MyOmit<T, K extends keyof T> = {
+//   [P in keyof T as P extends K ? never : P]: T[P];
+// };
+// type MyExclude<T, U> = T extends U ? never : T;
+// export type MyOmit<T, K extends keyof any> = {
+//   [P in MyExclude<keyof T, K>]: T[P];
+// };
+export type MyOmit<T, K extends keyof T> = {
+  [P in keyof T as P extends K ? never : P]: T[P];
+};
 interface Todo {
-  title: string
-  description: string
-  completed: boolean
+  readonly title: string;
+  description: string;
+  completed: boolean;
 }
 
-type TodoPreview = MyOmit<Todo, 'description' | 'title'>
+type TodoPreview = MyOmit<Todo, "description" | "completed">;
 
 const todo: TodoPreview = {
-  completed: false,
-}
+  title: "aa",
+};
+// todo.title = "bb";
--- type-challenges/day15_3-medium-omit/test-cases.ts
+++ type-challenges/day15_3-medium-omit/test-cases.ts
@@ -1,5 +1,8 @@
 import type { Equal, Expect } from "@type-challenges/utils";
 import type { MyOmit } from "./template";
+
+//  TypeScript 类型系统的一个重要概念：当我们使用工具类型（如 Omit）时，它们不仅仅是简单地挑选或排除属性，而是会创建一个全新的类型。这个新类型会重新设置所有属性的修饰符状态，而不是盲目地继承原始类型的修饰符。
+// 提供基础的、可组合的工具类型，让开发者能够根据具体需求灵活组合使用。
 type cases = [
   Expect<Equal<Expected1, MyOmit<Todo, "description">>>,
   Expect<Equal<Expected2, MyOmit<Todo, "description" | "completed">>>,
