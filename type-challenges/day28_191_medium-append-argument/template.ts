551f5c523b57110ead62e75733000f9b6579ab4c
Lenny <ly2484579947@gmail.com>
2024-12-01 09:43:17 +0800

    append-argument

---
+++ type-challenges/day28_191_medium-append-argument/template.ts
@@ -0,0 +1,11 @@
+// For given function type Fn, and any type A (any in this context means we don't restrict the type, and I don't have in mind any type 😉) create a generic type which will take Fn as the first argument, A as the second, and will produce function type G which will be the same as Fn but with appended argument A as a last one.
+export type AppendArgument<Fn extends (...args: any[]) => any, A> = Fn extends (
+  ...args: infer R
+) => infer Return
+  ? (...args: [...R, A]) => Return
+  : never;
+
+type Fn = (a: number, b: string) => number;
+
+type Result = AppendArgument<Fn, boolean>;
+// expected be (a: number, b: string, x: boolean) => number
